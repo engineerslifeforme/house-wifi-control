772004a772af3811128e88c5055bb464ace77210
Creative Rigor, LLC <creativerigor@gmail.com>
2022-11-13 20:47:44 -0500

    simple boilerplate

---
+++ app.py
@@ -0,0 +1,17 @@
+""" Dash app for wifi device control"""
+
+from dash import Dash, html, dcc
+
+app = Dash(__name__)
+
+app.layout = html.Div(children=[
+    html.H1(children='Hello Dash'),
+
+    html.Div(children='''
+        Dash: A web application framework for your data.
+    '''),
+
+])
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
